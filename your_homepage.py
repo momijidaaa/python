bf25f33e4194aa0936d10e6b90b9bc2314906ab6
momijidaaa <m89184412@gmail.com>
2025-10-02 16:29:33 +0000

    a

--- your_homepage.py
+++ your_homepage.py
@@ -10,14 +10,18 @@ st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXADv4Mi32xRaj4K
 
 # ボタン
 if st.button("押してみて"):
-   st.write("こんにちは！ボタンを押してくれてありがとう！")
+    st.write("こんにちは！ボタンを押してくれてありがとう！")
 if st.button("押さないで"):
-   st.write("なんで押したの？")
+    st.write("なんで押したの？")
 if st.button("810"):
-   st.write("淫夢厨なんですか？")
+    st.write("淫夢厨なんですか？")
+
 # フォーム
 name = st.text_input("あなたの名前は？")
 age = st.text_input("あなたの年齢は？")
+
 if st.button("送信"):
-    st.error("すべての項目を入力してください！")
-    st.write(f"{name}さん、こんにちは！ 君は{age}歳なんだね！") # https://python-ddvmuygaameabr7brnygeh.streamlit.app/
+    if not name or not age:   # どちらか空ならエラー
+        st.error("すべての項目を入力してください！")
+    else:
+        st.write(f"{name}さん、こんにちは！ 君は{age}歳なんだね！")

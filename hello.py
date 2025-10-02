760df735cfb83fc9be3690e0fd94ffa453593dad
momijidaaa <m89184412@gmail.com>
2025-10-02 16:21:44 +0000

    ん

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 # hello.py
 name = input("あなたの名前は？: ")
-print(f"こんにちは、{name}さん！Pythonへようこそ！")
+age = input("あなたの年齢は？: ")
+quit = input("Pythonを始めた理由は？: ")
+print(f"こんにちは、{name}さん！Pythonへようこそ！あなたは{age}歳ですね。ok, {quit}のためにPythonを始めたのですね！")
